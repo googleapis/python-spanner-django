googleapis/python-spanner-django

commit 8d19a4aaafbe54f006b6c049a6647505e870c206

dbapi: retrofit UPDATE statements with missing WHERE clause
Augments SQL statements missing a WHERE clause with

    WHERE 1=1

Thus for example:

    UPDATE (SELECT * FROM A JOIN c ON ai.id = c.id WHERE cl.ci = 1) SET d=5
    UPDATE T SET A = 1
    DELETE * from TABLE

become:

    UPDATE (SELECT * FROM A JOIN c ON ai.id = c.id WHERE cl.ci = 1) SET d=5 WHERE 1=1
    UPDATE T SET A = 1 WHERE 1=1
    DELETE * from TABLE WHERE 1=1

Fixes #94

spanner/django/operations.py
@@ -30,10 +30,9 @@ class DatabaseOperations(BaseDatabaseOperations):
         # Cloud Spanner doesn't support TRUNCATE so DELETE instead.
         # A dummy WHERE clause is required.
         if tables:
-            delete_sql = '%s %s %%s %s 1=1;' % (
+            delete_sql = '%s %s %%s;' % (
                 style.SQL_KEYWORD('DELETE'),
                 style.SQL_KEYWORD('FROM'),
-                style.SQL_KEYWORD('WHERE'),
             )
             return [
                 delete_sql % style.SQL_FIELD(self.quote_name(table))

setup.py
@@ -1,6 +1,7 @@
 from setuptools import find_packages, setup
 
 install_requires = [
+    'sqlparse >= 0.3.0',
     'google-cloud >= 0.34.0',
     'google-cloud-spanner >= 1.8.0',
 ]

spanner/dbapi/cursor.py
@@ -16,8 +16,9 @@ import google.api_core.exceptions as grpc_exceptions
 
 from .exceptions import IntegrityError, OperationalError, ProgrammingError
 from .parse_utils import (
-    STMT_DDL, STMT_INSERT, STMT_NON_UPDATING, classify_stmt, infer_param_types,
-    parse_insert, rows_for_insert_or_update, sql_pyformat_args_to_spanner,
+    STMT_DDL, STMT_INSERT, STMT_NON_UPDATING, classify_stmt,
+    ensure_where_clause, infer_param_types, parse_insert,
+    rows_for_insert_or_update, sql_pyformat_args_to_spanner,
 )
 
 _UNSET_COUNT = -1
@@ -108,6 +109,7 @@ class Cursor(object):
             raise OperationalError(e.details if hasattr(e, 'details') else e)
 
     def __do_execute_update(self, transaction, sql, params, param_types=None):
+        sql = ensure_where_clause(sql)
         sql, params = sql_pyformat_args_to_spanner(sql, params)
 
         # Given that we now format datetime as a Spanner TimeStamp,

spanner/dbapi/parse_utils.py
@@ -15,6 +15,7 @@
 import re
 from urllib.parse import urlparse
 
+import sqlparse
 from google.cloud import spanner_v1 as spanner
 
 from .exceptions import Error
@@ -421,6 +422,16 @@ def infer_param_types(params, param_types):
             param_types = insert_key_in_param_types(key, param_types, spanner.param_types.TIMESTAMP)
 
     return param_types
+
+
+def ensure_where_clause(sql):
+    """
+    Cloud Spanner requires a WHERE clause on UPDATE and DELETE statements.
+    Add a dummy WHERE clause if necessary.
+    """
+    if any(isinstance(token, sqlparse.sql.Where) for token in sqlparse.parse(sql)[0]):
+        return sql
+    return sql + ' WHERE 1=1'
 
 
 def insert_key_in_param_types(key, param_types, spanner_type):

tests/spanner/dbapi/test_parse_utils.py
@@ -18,9 +18,10 @@ from google.cloud import spanner_v1 as spanner
 from spanner.dbapi.exceptions import Error
 from spanner.dbapi.parse_utils import (
     STMT_DDL, STMT_NON_UPDATING, TimestampStr, classify_stmt,
-    extract_connection_params, infer_param_types, parse_insert,
-    parse_spanner_url, reINSTANCE_CONFIG, rows_for_insert_or_update,
-    sql_pyformat_args_to_spanner, validate_instance_config,
+    ensure_where_clause, extract_connection_params, infer_param_types,
+    parse_insert, parse_spanner_url, reINSTANCE_CONFIG,
+    rows_for_insert_or_update, sql_pyformat_args_to_spanner,
+    validate_instance_config,
 )
 
 
@@ -367,3 +368,36 @@ class ParseUtilsTests(TestCase):
             with self.subTest(i=i):
                 got_param_types = infer_param_types(params, param_types)
                 self.assertEqual(got_param_types, want_param_types)
+
+    def test_ensure_where_clause(self):
+        cases = [
+                (
+                    'UPDATE a SET a.b=10 FROM articles a JOIN d c ON a.ai = c.ai WHERE c.ci = 1',
+                    'UPDATE a SET a.b=10 FROM articles a JOIN d c ON a.ai = c.ai WHERE c.ci = 1',
+                ),
+                (
+                    'UPDATE (SELECT * FROM A JOIN c ON ai.id = c.id WHERE cl.ci = 1) SET d=5',
+                    'UPDATE (SELECT * FROM A JOIN c ON ai.id = c.id WHERE cl.ci = 1) SET d=5 WHERE 1=1',
+                ),
+                (
+                    'UPDATE T SET A = 1 WHERE C1 = 1 AND C2 = 2',
+                    'UPDATE T SET A = 1 WHERE C1 = 1 AND C2 = 2',
+                ),
+                (
+                    'UPDATE T SET r=r*0.9 WHERE id IN (SELECT id FROM items WHERE r / w >= 1.3 AND q > 100)',
+                    'UPDATE T SET r=r*0.9 WHERE id IN (SELECT id FROM items WHERE r / w >= 1.3 AND q > 100)',
+                ),
+                (
+                    'UPDATE T SET r=r*0.9 WHERE id IN (SELECT id FROM items WHERE r / w >= 1.3 AND q > 100)',
+                    'UPDATE T SET r=r*0.9 WHERE id IN (SELECT id FROM items WHERE r / w >= 1.3 AND q > 100)',
+                ),
+                (
+                    'DELETE * FROM TABLE',
+                    'DELETE * FROM TABLE WHERE 1=1',
+                ),
+        ]
+
+        for sql, want in cases:
+            with self.subTest(sql=sql):
+                got = ensure_where_clause(sql)
+                self.assertEqual(got, want)

commit fef9cc61db761135c0d5766d9866a3369c58fbbe

fix improper WHERE clause insertion in sql_flush
fixes #113 (regression in #111)

spanner/django/operations.py
@@ -30,7 +30,7 @@ class DatabaseOperations(BaseDatabaseOperations):
         # Cloud Spanner doesn't support TRUNCATE so DELETE instead.
         # A dummy WHERE clause is required.
         if tables:
-            delete_sql = '%s %s %%s;' % (
+            delete_sql = '%s %s %%s' % (
                 style.SQL_KEYWORD('DELETE'),
                 style.SQL_KEYWORD('FROM'),
             )
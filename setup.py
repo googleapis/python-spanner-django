596c107bd0611c3befead81cd56fcba4a7aa8aad
larkee <larkee@users.noreply.github.com>
2020-05-26 11:12:49 +1000

    add missing io import

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@
 # license that can be found in the LICENSE file or at
 # https://developers.google.com/open-source/licenses/bsd
 
+import io
 import os
 
 from setuptools import find_packages, setup

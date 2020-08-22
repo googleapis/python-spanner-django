4125aa23bceda68ba608e533c4b090a52d8c788c
STATION\mf <mf2199@columbia.edu>
2020-08-22 15:06:52 -0400

    fix: `BSD` liecense replaced with `Apache 2.0`

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
 # Copyright 2020 Google LLC
 #
-# Use of this source code is governed by a BSD-style
-# license that can be found in the LICENSE file or at
-# https://developers.google.com/open-source/licenses/bsd
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import io
 import os
@@ -42,7 +50,7 @@ setup(
         long_description=readme,
         author='Google LLC',
         author_email='cloud-spanner-developers@googlegroups.com',
-        license='BSD',
+        license='Apache 2.0',
         packages=find_packages(exclude=['tests']),
         install_requires=dependencies,
         url="https://github.com/googleapis/python-spanner-django",
@@ -50,7 +58,7 @@ setup(
             release_status,
             'Environment :: Web Environment',
             'Intended Audience :: Developers',
-            'License :: OSI Approved :: BSD',
+            'License :: OSI Approved :: Apache Software License',
             'Operating System :: OS Independent',
             'Programming Language :: Python :: 3',
             'Programming Language :: Python :: 3.5',

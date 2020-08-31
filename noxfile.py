7d9b5718a0e32e96da6fb9f98e5a3fc8ad9df699
STATION\mf <mf2199@columbia.edu>
2020-08-31 17:12:42 -0400

    feat: added system test target + test utils package

--- noxfile.py
+++ noxfile.py
@@ -88,6 +88,41 @@ def unit(session):
 
 
 @nox.session(python="3.8")
+def system(session):
+    """Run the system test suite."""
+    system_test_path = os.path.join("tests", "system.py")
+    system_test_folder_path = os.path.join("tests", "system")
+
+    # Sanity check: Only run tests if the environment variable is set.
+    if not os.environ.get("GOOGLE_APPLICATION_CREDENTIALS", ""):
+        session.skip("Credentials must be set via environment variable")
+
+    system_test_exists = os.path.exists(system_test_path)
+    system_test_folder_exists = os.path.exists(system_test_folder_path)
+
+    # Sanity check: only run tests if found.
+    if not system_test_exists and not system_test_folder_exists:
+        session.skip("System tests were not found")
+
+    # Install dependencies, e.g.:
+    # session.install("google-cloud-iam")
+    # Use pre-release gRPC for system tests.
+    # session.install("--pre", "grpcio")
+
+    # Install all test dependencies, then install this package into the
+    # virtualenv's dist-packages.
+    session.install("mock", "pytest")
+    session.install("-e", ".")
+    session.install("-e", "test_utils")
+
+    # Run py.test against the system tests.
+    if system_test_exists:
+        session.run("py.test", "--quiet", system_test_path, *session.posargs)
+    if system_test_folder_exists:
+        session.run("py.test", "--quiet", system_test_folder_path, *session.posargs)
+
+
+@nox.session(python="3.8")
 def cover(session):
     """Run the final coverage report.
 

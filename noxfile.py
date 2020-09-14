01a39410ca22abb3b9643f70a2023d2af256d2ca
STATION\mf <mf2199@columbia.edu>
2020-09-13 22:05:06 -0400

    test: baseline (repeat)

--- noxfile.py
+++ noxfile.py
@@ -126,7 +126,7 @@ def cover(session):
     test runs (not system test runs), and then erases coverage data.
     """
     session.install("coverage", "pytest-cov")
-    session.run("coverage", "report", "--show-missing", "--fail-under=30")
+    session.run("coverage", "report", "--show-missing", "--fail-under=40")
     session.run("coverage", "erase")
 
 

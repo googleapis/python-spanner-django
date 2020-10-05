886d7bf383d799225cfe2b13b20b5fef64b05f15
IlyaFaer <ilya.faer@mail.ru>
2020-10-05 11:07:26 +0300

    test: don't use Django fork for testing

--- .kokoro/build.sh
+++ .kokoro/build.sh
@@ -57,7 +57,7 @@ pip3 install .
 # Create a unique DJANGO_TESTS_DIR per worker to avoid
 # any clashes with configured tests by other workers.
 export DJANGO_TESTS_DIR="django_tests_$DJANGO_WORKER_INDEX"
-mkdir -p $DJANGO_TESTS_DIR && git clone --depth 1 --single-branch --branch spanner-2.2.x https://github.com/timgraham/django.git $DJANGO_TESTS_DIR/django
+mkdir -p $DJANGO_TESTS_DIR && git clone --depth 1 --single-branch --branch master https://github.com/django/django.git $DJANGO_TESTS_DIR/django
 
 # Install dependencies for Django tests.
 sudo apt-get update

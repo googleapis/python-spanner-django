6b3a14eacdfcb0d2b438339791976054c3874a31
STATION\MF <mf2199@columbia.edu>
2020-10-05 19:59:40 -0400

    wip: some refactoring on `cursor.py`

--- google/cloud/spanner_dbapi/parse_utils.py
+++ google/cloud/spanner_dbapi/parse_utils.py
@@ -4,7 +4,7 @@
 # license that can be found in the LICENSE file or at
 # https://developers.google.com/open-source/licenses/bsd
 
-"SQL parsing and classification utils."
+"""SQL parsing and classification utils."""
 
 import datetime
 import decimal

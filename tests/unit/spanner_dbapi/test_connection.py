81db6f73a9c652b3238f1c93886465beef78d5fd
STATION\MF <mf2199@columbia.edu>
2020-10-23 21:41:03 -0400

    test: unit test for `Connection._session_checkout`

--- tests/unit/spanner_dbapi/test_connection.py
+++ tests/unit/spanner_dbapi/test_connection.py
@@ -61,6 +61,24 @@ class TestConnection(unittest.TestCase):
             mock_commit.assert_not_called()
             self.assertEqual(connection._autocommit, False)
 
+    def test__session_checkout(self):
+        from google.cloud.spanner_dbapi import Connection
+
+        with mock.patch(
+            "google.cloud.spanner_v1.database.Database",
+        ) as mock_database:
+            mock_database._pool = mock.MagicMock()
+            mock_database._pool.get = mock.MagicMock(return_value='db_session_pool')
+            connection = Connection(self.INSTANCE, mock_database)
+
+            connection._session_checkout()
+            mock_database._pool.get.assert_called_once_with()
+            self.assertEqual(connection._session, 'db_session_pool')
+
+            connection._session = 'db_session'
+            connection._session_checkout()
+            self.assertEqual(connection._session, 'db_session')
+
     def test_close(self):
         from google.cloud.spanner_dbapi import connect, InterfaceError
 

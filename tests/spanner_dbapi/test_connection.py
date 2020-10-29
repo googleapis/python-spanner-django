3c3572b57fa6d9b950beb95de1b1b72d9bd55411
Tina Smirnova <tinanight98@gmail.com>
2020-10-29 12:58:29 +0300

    revert test

--- tests/spanner_dbapi/test_connection.py
+++ tests/spanner_dbapi/test_connection.py
@@ -10,19 +10,16 @@ import unittest
 from unittest import mock
 
 # import google.cloud.spanner_dbapi.exceptions as dbapi_exceptions
-from google.cloud.spanner_v1.transaction import Transaction
 
 from google.cloud.spanner_dbapi import Connection, InterfaceError
 from google.cloud.spanner_dbapi.connection import AUTOCOMMIT_MODE_WARNING
 from google.cloud.spanner_v1.database import Database
 from google.cloud.spanner_v1.instance import Instance
-from google.cloud.spanner_v1.database import SnapshotCheckout
 
 
 class TestConnection(unittest.TestCase):
     instance_name = "instance-name"
     database_name = "database-name"
-    ddl_statements = ["ALTER TABLE TestTable ADD COLUMN Column STRING(1024)"]
 
     def _make_connection(self):
         # we don't need real Client object to test the constructor
@@ -40,59 +37,6 @@ class TestConnection(unittest.TestCase):
         self.assertEqual(connection.database.database_id, self.database_name)
 
         self.assertFalse(connection.is_closed)
-
-    def test_read_snapshot(self):
-        connection = self._make_connection()
-
-        self.assertIsInstance(connection.read_snapshot(), SnapshotCheckout)
-    #     TODO check smth
-
-    def test_in_transaction(self):
-        import datetime
-        from google.cloud.spanner_v1.proto.spanner_pb2 import CommitResponse
-        from google.cloud.spanner_v1.proto.transaction_pb2 import (
-            Transaction as TransactionPB,
-            TransactionOptions,
-        )
-        from google.cloud._helpers import UTC
-        from google.cloud._helpers import _datetime_to_pb_timestamp
-
-        connection = self._make_connection()
-
-        TABLE_NAME = "citizens"
-        COLUMNS = ["email", "first_name", "last_name", "age"]
-        VALUES = [
-            ["phred@exammple.com", "Phred", "Phlyntstone", 32],
-            ["bharney@example.com", "Bharney", "Rhubble", 31],
-        ]
-
-        TRANSACTION_ID = b"FACEDACE"
-        transaction_pb = TransactionPB(id=TRANSACTION_ID)
-        now = datetime.datetime.utcnow().replace(tzinfo=UTC)
-        now_pb = _datetime_to_pb_timestamp(now)
-        response = CommitResponse(commit_timestamp=now_pb)
-        # gax_api = self._make_spanner_api()
-        # gax_api.begin_transaction.return_value = transaction_pb
-        # gax_api.commit.return_value = response
-        # database = self._make_database()
-        # database.spanner_api = gax_api
-        # session = self._make_one(database)
-        # session._session_id = self.SESSION_ID
-
-        called_with = []
-        def unit_of_work(txn, *args, **kw):
-            called_with.append((txn, args, kw))
-            txn.insert(TABLE_NAME, COLUMNS, VALUES)
-            return 42
-
-        return_value = connection.in_transaction(unit_of_work, "abc", some_arg="def")
-
-        # txn, args, kw = called_with[0]
-        # self.assertIsInstance(txn, Transaction)
-        # self.assertEqual(return_value, 42)
-        # self.assertEqual(args, ("abc",))
-        # self.assertEqual(kw, {"some_arg": "def"})
-
 
     def test_close(self):
         connection = self._make_connection()

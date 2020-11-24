2ca10058b0c7f838763f6ec2ee5fcf96741d0087
Chris Kleinknecht <libc@google.com>
2020-11-23 19:23:54 -0800

    Remove DBAPI system tests

--- noxfile.py
+++ noxfile.py
@@ -88,44 +88,11 @@ def unit(session):
 
 
 @nox.session(python="3.8")
-def system(session):
-    """Run the system test suite."""
-    system_test_path = os.path.join("tests", "system.py")
-    system_test_folder_path = os.path.join("tests", "system")
-
-    # Sanity check: Only run tests if the environment variable is set.
-    if not os.environ.get(
-        "GOOGLE_APPLICATION_CREDENTIALS", ""
-    ) and not os.environ.get("SPANNER_EMULATOR_HOST", ""):
-        session.skip("Credentials must be set via environment variable")
-
-    system_test_exists = os.path.exists(system_test_path)
-    system_test_folder_exists = os.path.exists(system_test_folder_path)
-
-    # Sanity check: only run tests if found.
-    if not system_test_exists and not system_test_folder_exists:
-        session.skip("System tests were not found")
-
-    # Install all test dependencies, then install this package into the
-    # virtualenv's dist-packages.
-    session.install("mock", "pytest", "google-cloud-testutils")
-    session.install("-e", ".")
-
-    # Run py.test against the system tests.
-    if system_test_exists:
-        session.run("py.test", "--quiet", system_test_path, *session.posargs)
-    if system_test_folder_exists:
-        session.run(
-            "py.test", "--quiet", system_test_folder_path, *session.posargs
-        )
-
-
-@nox.session(python="3.8")
 def cover(session):
     """Run the final coverage report.
 
-    This outputs the coverage report aggregating coverage from the unit
-    test runs (not system test runs), and then erases coverage data.
+    This outputs the coverage report aggregating coverage from the unit test
+    runs and then erases coverage data.
     """
     session.install("coverage", "pytest-cov")
     session.run("coverage", "report", "--show-missing", "--fail-under=40")

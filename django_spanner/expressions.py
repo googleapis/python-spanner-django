1a6257ab2cb434e0b15aab33a9309d9d383924c3
Tina Smirnova <tinanight98@gmail.com>
2020-11-26 12:35:08 +0300

    docs for expressions

--- django_spanner/expressions.py
+++ django_spanner/expressions.py
@@ -8,6 +8,13 @@ from django.db.models.expressions import OrderBy
 
 
 def order_by(self, compiler, connection, **extra_context):
+    """Order expressions in the SQL query.
+
+    TODO: Make this function a part of the class.
+
+    :rtype: str
+    :returns: A SQL query.
+    """
     # In Django 3.1, this can be replaced with
     # DatabaseFeatures.supports_order_by_nulls_modifier = False.
     template = None
@@ -21,4 +28,5 @@ def order_by(self, compiler, connection, **extra_context):
 
 
 def register_expressions():
+    """Register ordered expressions in Spanner."""
     OrderBy.as_spanner = order_by

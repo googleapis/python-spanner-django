1dbc429a00326c9f1a715c8b21e93f6e87e9a6b7
IlyaFaer <ilya.faer@mail.ru>
2020-12-04 13:37:48 +0300

    re-run

--- django_test_suite.sh
+++ django_test_suite.sh
@@ -4,8 +4,7 @@
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
-# exit when any command fails
-set -e
+set -x pipefail
 
 # Disable buffering, so that the logs stream through.
 export PYTHONUNBUFFERED=1

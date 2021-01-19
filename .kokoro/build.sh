18d7398f8542acd43451a78a2e0aff10d60858e9
IlyaFaer <ilya.faer@mail.ru>
2021-01-19 10:39:22 +0300

    use another env var name

--- .kokoro/build.sh
+++ .kokoro/build.sh
@@ -27,7 +27,7 @@ env | grep KOKORO
 export GOOGLE_APPLICATION_CREDENTIALS=${KOKORO_GFILE_DIR}/service-account.json
 
 # Setup project id.
-export PROJECT_ID=$(cat "${KOKORO_GFILE_DIR}/project-id.json")
+export GOOGLE_CLOUD_PROJECT=$(cat "${KOKORO_GFILE_DIR}/project-id.json")
 
 # Remove old nox
 python3.6 -m pip uninstall --yes --quiet nox-automation

495ef6856760b8dea28d17e849797a7f49b4c1f3
IlyaFaer <ilya.faer@mail.ru>
2021-01-19 15:28:51 +0300

    use more workers

--- .kokoro/build.sh
+++ .kokoro/build.sh
@@ -78,7 +78,7 @@ then
         # 'spanner.googleapis.com').
         export DJANGO_WORKER_COUNT=0
     else
-        export DJANGO_WORKER_COUNT=1
+        export DJANGO_WORKER_COUNT=2
     fi
 else
     export DJANGO_WORKER_COUNT=$(ls .kokoro/presubmit/worker* | wc -l)

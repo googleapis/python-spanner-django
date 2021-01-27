08fb72622dc444044653333c3aa4af020c5482ae
Chris Kleinknecht <libc@google.com>
2021-01-26 19:48:17 -0800

    Don't set -x, do set -o pipefail

--- django_test_suite.sh
+++ django_test_suite.sh
@@ -4,7 +4,7 @@
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
-set -x pipefail
+set -eo pipefail
 
 sudo apt-get update -y
 sudo apt-get install -y libmemcached-dev

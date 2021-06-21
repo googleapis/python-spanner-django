b6c746fa6dc3e538a77bbe6c53e890152c5ef401
Bu Sun Kim <busunkim@google.com>
2021-06-21 16:14:57 +0000

    chore: add populate secrets script

--- owlbot.py
+++ owlbot.py
@@ -27,7 +27,7 @@ s.move(templated_files / ".kokoro" / "docs")
 s.move(templated_files / ".kokoro" / "release.sh")
 s.move(templated_files / ".kokoro" / "trampoline_v2.sh")
 s.move(templated_files / ".kokoro" / "trampoline.sh")
-s.move(templated_files / ".kokoro" / "populate_secrets.sh")
+s.move(templated_files / ".kokoro" / "populate-secrets.sh")
 s.move(templated_files / ".kokoro" / "release")
 
 s.shell.run(["nox", "-s", "blacken"], hide_output=False)

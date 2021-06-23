a191fdda33080cd768be1ea4576a9dd49fb8dbce
Bu Sun Kim <busunkim@google.com>
2021-06-23 23:49:27 +0000

    docs: fix license

--- owlbot.py
+++ owlbot.py
@@ -1,18 +1,12 @@
 # Copyright 2021 Google LLC
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file or at
+# https://developers.google.com/open-source/licenses/bsd
 
 """This script is used to synthesize generated parts of this library."""
+import re
+
 import synthtool as s
 import synthtool.gcp as gcp
 
@@ -29,5 +23,18 @@ s.move(templated_files / ".kokoro" / "trampoline_v2.sh")
 s.move(templated_files / ".kokoro" / "trampoline.sh")
 s.move(templated_files / ".kokoro" / "populate-secrets.sh")
 s.move(templated_files / ".kokoro" / "release")
+
+# Replace the Apache Licenses in the `.kokoro` directory
+# with the BSD license expected in this repository
+s.replace(
+    ".kokoro/**/*",
+    "# Copyright.*Google LLC.*# limitations under the License\.",
+    """# Copyright 2021 Google LLC
+#
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file or at
+# https://developers.google.com/open-source/licenses/bsd""",
+    flags=re.DOTALL
+)
 
 s.shell.run(["nox", "-s", "blacken"], hide_output=False)

3eba2985c70082951d389f2954b1fe57e0a26b47
Bu Sun Kim <busunkim@google.com>
2021-07-22 00:09:02 +0000

    chore: add .kokoro/docker directory

--- owlbot.py
+++ owlbot.py
@@ -18,6 +18,7 @@ templated_files = gcp.CommonTemplates().py_library(microgenerator=True)
 # Just move templates for building docs and releases
 # Presubmit and continuous are configured differently
 s.move(templated_files / ".trampolinerc")
+s.move(templated_files / ".kokoro" / "docker")
 s.move(templated_files / ".kokoro" / "docs")
 s.move(templated_files / ".kokoro" / "release.sh")
 s.move(templated_files / ".kokoro" / "trampoline_v2.sh")

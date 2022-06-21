f9097c6d1ee6cc58cd01f3119f3de56cec1934b5
IlyaFaer <gurovilya9@gmail.com>
2022-06-21 12:10:13 +0400

    split batches into smaller chunks

--- django_spanner/__init__.py
+++ django_spanner/__init__.py
@@ -14,10 +14,13 @@ from uuid import uuid4
 
 import pkg_resources
 from google.cloud.spanner_v1 import JsonObject
+from django.db import connection as connection_db, connections, transaction
 from django.db.models.fields import (
     AutoField,
     Field,
 )
+from django.db.models.query import QuerySet as _QuerySet
+from django.utils.functional import partition
 
 from .expressions import register_expressions
 from .functions import register_functions
@@ -51,6 +54,126 @@ check_django_compatability(SUPPORTED_DJANGO_VERSIONS)
 register_expressions(USING_DJANGO_3)
 register_functions()
 register_lookups()
+
+
+class QuerySet(_QuerySet):
+    def bulk_create(
+        self,
+        objs,
+        batch_size=None,
+        ignore_conflicts=False,
+        update_conflicts=False,
+        update_fields=None,
+        unique_fields=None,
+    ):
+        """
+        Insert each of the instances into the database. Do *not* call
+        save() on each of the instances, do not send any pre/post_save
+        signals, and do not set the primary key attribute if it is an
+        autoincrement field (except if features.can_return_rows_from_bulk_insert=True).
+        Multi-table models are not supported.
+        """
+        # When you bulk insert you don't get the primary keys back (if it's an
+        # autoincrement, except if can_return_rows_from_bulk_insert=True), so
+        # you can't insert into the child tables which references this. There
+        # are two workarounds:
+        # 1) This could be implemented if you didn't have an autoincrement pk
+        # 2) You could do it by doing O(n) normal inserts into the parent
+        #    tables to get the primary keys back and then doing a single bulk
+        #    insert into the childmost table.
+        # We currently set the primary keys on the objects when using
+        # PostgreSQL via the RETURNING ID clause. It should be possible for
+        # Oracle as well, but the semantics for extracting the primary keys is
+        # trickier so it's not done yet.
+        if batch_size is not None and batch_size <= 0:
+            raise ValueError("Batch size must be a positive integer.")
+        # Check that the parents share the same concrete model with the our
+        # model to detect the inheritance pattern ConcreteGrandParent ->
+        # MultiTableParent -> ProxyChild. Simply checking self.model._meta.proxy
+        # would not identify that case as involving multiple tables.
+        for parent in self.model._meta.get_parent_list():
+            if (
+                parent._meta.concrete_model
+                is not self.model._meta.concrete_model
+            ):
+                raise ValueError(
+                    "Can't bulk create a multi-table inherited model"
+                )
+        if not objs:
+            return objs
+
+        on_conflict = self._check_bulk_create_options(
+            ignore_conflicts,
+            update_conflicts,
+            update_fields,
+            unique_fields,
+        )
+        self._for_write = True
+        opts = self.model._meta
+        fields = opts.concrete_fields
+        objs = list(objs)
+        self._prepare_for_bulk_create(objs)
+
+        batch_size = connection_db.features.max_query_params
+
+        with transaction.atomic(using=self.db, savepoint=False):
+            objs_with_pk, objs_without_pk = partition(
+                lambda o: o.pk is None, objs
+            )
+            if objs_with_pk:
+
+                for chunk_with_pk in [
+                    objs_with_pk[x : x + batch_size]
+                    for x in range(0, len(objs_with_pk), batch_size)
+                ]:
+                    returned_columns = self._batched_insert(
+                        chunk_with_pk,
+                        fields,
+                        batch_size,
+                        on_conflict=on_conflict,
+                        update_fields=update_fields,
+                        unique_fields=unique_fields,
+                    )
+                    for obj_with_pk, results in zip(
+                        chunk_with_pk, returned_columns
+                    ):
+                        for result, field in zip(
+                            results, opts.db_returning_fields
+                        ):
+                            if field != opts.pk:
+                                setattr(obj_with_pk, field.attname, result)
+
+                    for obj_with_pk in chunk_with_pk:
+                        obj_with_pk._state.adding = False
+                        obj_with_pk._state.db = self.db
+
+            if objs_without_pk:
+                fields = [f for f in fields if not isinstance(f, AutoField)]
+                returned_columns = self._batched_insert(
+                    objs_without_pk,
+                    fields,
+                    batch_size,
+                    on_conflict=on_conflict,
+                    update_fields=update_fields,
+                    unique_fields=unique_fields,
+                )
+                connection = connections[self.db]
+                if (
+                    connection.features.can_return_rows_from_bulk_insert
+                    and on_conflict is None
+                ):
+                    assert len(returned_columns) == len(objs_without_pk)
+                for obj_without_pk, results in zip(
+                    objs_without_pk, returned_columns
+                ):
+                    for result, field in zip(
+                        results, opts.db_returning_fields
+                    ):
+                        setattr(obj_without_pk, field.attname, result)
+                    obj_without_pk._state.adding = False
+                    obj_without_pk._state.db = self.db
+
+        return objs
 
 
 def gen_rand_int64():

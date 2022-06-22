39072cc96c86cfb6d09a59f932bf829321344945
IlyaFaer <gurovilya9@gmail.com>
2022-06-22 11:00:02 +0400

    fix

--- django_spanner/__init__.py
+++ django_spanner/__init__.py
@@ -26,6 +26,7 @@ from django.db.models.fields import (
 )
 from django.db.models.query import QuerySet
 from django.utils.functional import partition
+from django.contrib.contenttypes import ContentType
 
 from .expressions import register_expressions
 from .functions import register_functions
@@ -166,8 +167,66 @@ def spanner_prepare_for_bulk_create(self, objs):
         obj._prepare_related_fields_for_save(operation_name="bulk_create")
 
 
+def spanner_prepare_related_fields_for_save(self, operation_name, fields=None):
+    # Ensure that a model instance without a PK hasn't been assigned to
+    # a ForeignKey, GenericForeignKey or OneToOneField on this model. If
+    # the field is nullable, allowing the save would result in silent data
+    # loss.
+    for field in self._meta.concrete_fields:
+        if fields and field not in fields:
+            continue
+        # If the related field isn't cached, then an instance hasn't been
+        # assigned and there's no need to worry about this check.
+        if field.is_relation and field.is_cached(self):
+            obj = getattr(self, field.name, None)
+            if not obj:
+                continue
+            # A pk may have been assigned manually to a model instance not
+            # saved to the database (or auto-generated in a case like
+            # UUIDField), but we allow the save to proceed and rely on the
+            # database to raise an IntegrityError if applicable. If
+            # constraints aren't supported by the database, there's the
+            # unavoidable risk of data corruption.
+            if obj.pk is None:
+                # Remove the object from a related instance cache.
+                if not field.remote_field.multiple:
+                    field.remote_field.delete_cached_value(obj)
+                raise ValueError(
+                    "%s() prohibited to prevent data loss due to unsaved "
+                    "related object '%s'." % (operation_name, field.name)
+                )
+            elif getattr(self, field.attname) in field.empty_values:
+                # Set related object if it has been saved after an
+                # assignment.
+                setattr(self, field.name, obj)
+            # If the relationship's pk/to_field was changed, clear the
+            # cached relationship.
+            if getattr(obj, field.target_field.attname) != getattr(
+                self, field.attname
+            ):
+                field.delete_cached_value(self)
+    # GenericForeignKeys are private.
+    for field in self._meta.private_fields:
+        if fields and field not in fields:
+            continue
+        if (
+            field.is_relation
+            and field.is_cached(self)
+            and hasattr(field, "fk_field")
+        ):
+            obj = field.get_cached_value(self, default=None)
+            if obj and obj.pk is None:
+                raise ValueError(
+                    f"{operation_name}() prohibited to prevent data loss due to "
+                    f"unsaved related object '{field.name}'."
+                )
+
+
 QuerySet.bulk_create = spanner_bulk_create
 QuerySet._prepare_for_bulk_create = spanner_prepare_for_bulk_create
+ContentType._prepare_related_fields_for_save = (
+    spanner_prepare_related_fields_for_save
+)
 
 
 def gen_rand_int64():

1f6500f2a737d7ad647202f53454726de51921fc
IlyaFaer <gurovilya9@gmail.com>
2022-06-22 11:35:09 +0400

    fix

--- django_spanner/__init__.py
+++ django_spanner/__init__.py
@@ -99,7 +99,7 @@ def spanner_create_permissions(
     ]
     from django.forms.models import model_to_dict
 
-    raise ValueError(str(model_to_dict(perms[0])))
+    raise ValueError(str(model_to_dict(perms[1])))
     Permission.objects.using(using).bulk_create(perms, batch_size=900)
     if verbosity >= 2:
         for perm in perms:

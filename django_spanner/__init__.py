c96edd318cdad948ed01db3d5b90d5fa16fe9ecd
IlyaFaer <gurovilya9@gmail.com>
2022-06-22 11:17:00 +0400

    fix

--- django_spanner/__init__.py
+++ django_spanner/__init__.py
@@ -24,10 +24,86 @@ from .functions import register_functions
 from .lookups import register_lookups
 from .utils import check_django_compatability
 
+from django.contrib.auth import management
+
 # Monkey-patch google.DatetimeWithNanoseconds's __eq__ compare against
 # datetime.datetime.
 from google.api_core.datetime_helpers import DatetimeWithNanoseconds
 
+
+def spanner_create_permissions(
+    app_config,
+    verbosity=2,
+    interactive=True,
+    using=management.DEFAULT_DB_ALIAS,
+    apps=management.global_apps,
+    **kwargs
+):
+    if not app_config.models_module:
+        return
+
+    # Ensure that contenttypes are created for this app. Needed if
+    # 'django.contrib.auth' is in INSTALLED_APPS before
+    # 'django.contrib.contenttypes'.
+    management.create_contenttypes(
+        app_config,
+        verbosity=verbosity,
+        interactive=interactive,
+        using=using,
+        apps=apps,
+        **kwargs
+    )
+
+    app_label = app_config.label
+    try:
+        app_config = apps.get_app_config(app_label)
+        ContentType = apps.get_model("contenttypes", "ContentType")
+        Permission = apps.get_model("auth", "Permission")
+    except LookupError:
+        return
+
+    if not management.router.allow_migrate_model(using, Permission):
+        return
+
+    # This will hold the permissions we're looking for as
+    # (content_type, (codename, name))
+    searched_perms = []
+    # The codenames and ctypes that should exist.
+    ctypes = set()
+    for klass in app_config.get_models():
+        # Force looking up the content types in the current database
+        # before creating foreign keys to them.
+        ctype = ContentType.objects.db_manager(using).get_for_model(
+            klass, for_concrete_model=False
+        )
+
+        ctypes.add(ctype)
+        for perm in management._get_all_permissions(klass._meta):
+            searched_perms.append((ctype, perm))
+
+    # Find all the Permissions that have a content_type for a model we're
+    # looking for.  We don't need to check for codenames since we already have
+    # a list of the ones we're going to create.
+    all_perms = set(
+        Permission.objects.using(using)
+        .filter(
+            content_type__in=ctypes,
+        )
+        .values_list("content_type", "codename")
+    )
+
+    perms = [
+        Permission(codename=codename, name=name, content_type=ct)
+        for ct, (codename, name) in searched_perms
+        if (ct.pk, codename) not in all_perms
+    ]
+    Permission.objects.using(using).bulk_create(perms, batch_size=900)
+    if verbosity >= 2:
+        for perm in perms:
+            print("Adding permission '%s'" % perm)
+
+
+management.create_permissions = spanner_create_permissions
 
 USING_DJANGO_3 = False
 if django.VERSION[:2] == (3, 2):

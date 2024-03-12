b4b99543cca29805e09a7e54c099b43bc564e854
ankiaga <ankiaga@google.com>
2024-03-12 14:55:17 +0530

    More changes

--- django_test_suite_3.2.sh
+++ django_test_suite_3.2.sh
@@ -18,7 +18,7 @@ mkdir -p $DJANGO_TESTS_DIR
 if [ $SPANNER_EMULATOR_HOST != 0 ]
 then
     pip3 install .
-    git clone --depth 1 --single-branch --branch "django/stable/4.2.x" https://github.com/django/django.git $DJANGO_TESTS_DIR/django3.2
+    git clone --depth 1 --single-branch --branch "django/stable/4.2.x" https://github.com/googleapis/python-spanner-django.git $DJANGO_TESTS_DIR/django3.2
 fi
 
 # Install dependencies for Django tests.
